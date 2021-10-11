2018e5c2c4b1a633a6c6a4d06f8cce116b2f4c26
Daniel Roelfs <daniel@roelfs.net>
2021-10-11 14:38:21 +0200

    add pointer plugin

--- dist/reveal-init.js
+++ dist/reveal-init.js
@@ -56,7 +56,15 @@
 			// 	37: 'prev'	// left key
 			// },
 
-			plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ],
+			pointer: {
+				key: "q", // key to enable pointer, default "q", not case-sensitive
+				color: "red", // color of a cursor, default "red" any valid CSS color
+				pointerSize: 16, // pointer size in px, default 12
+				alwaysVisible: false, // should pointer mode be always visible? default "false"
+				tailLength: 10, // NOT IMPLEMENTED YET!!! how long the "tail" should be? default 10
+			  },
+
+			plugins: [ RevealMarkdown, RevealHighlight, RevealNotes, RevealPointer ],
 
 			/**dependencies: [
 					{ src: 'plugin/markdown/markdown.js' },
